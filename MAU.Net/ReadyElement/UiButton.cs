b31e33b12771d3e63b62373ec3ac692198a9f63f
CorrM <islamnofl.official@gmail.com>
2020-03-19 03:02:39 +0200

    #

--- MAU.Net/ReadyElement/UiButton.cs
+++ MAU.Net/ReadyElement/UiButton.cs
@@ -6,16 +6,12 @@ namespace MAU.ReadyElement
 {
 	public class UiButton : UiElement
 	{
-		[UiProperty("data-gg")]
-		public string Gg { get; set; }
-
 		public UiButton(string id) : base(id) { }
 
 		[UiEvent("click")]
 		public void OnClick(string eventType, JObject eventData)
 		{
 			Debug.WriteLine("Click Event Called");
-			Debug.WriteLine($"Gg => {Gg}");
 			Html = "<b>CorrM</b>";
 		}
 	}
