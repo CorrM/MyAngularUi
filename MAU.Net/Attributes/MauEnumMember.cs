85a45ca279f9c03af6c8fa375649b341e14304b0
CorrM <islamnofl.official@gmail.com>
2020-08-28 07:32:17 +0200

    - Fix bugs on `MauEnumMember`.

--- MAU.Net/Attributes/MauEnumMember.cs
+++ MAU.Net/Attributes/MauEnumMember.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -45,9 +46,8 @@ namespace MAU.Attributes
 		public static bool HasNotSetValue(Type enumType)
 		{
 			// NotSet must to be in any MauProperty value is `Enum`
-			// And also must to be first value in Enum
-			// For sure will equal == 0
-			return Enum.GetNames(enumType)[0] == "NotSet";
+			string[] names = Enum.GetNames(enumType);
+			return names.Contains("NotSet");
 		}
 		public static bool GetValidEnumValue(Type valueType, ref object originalValue)
 		{
@@ -55,20 +55,33 @@ namespace MAU.Attributes
 				return false;
 
 			if (!MauEnumMember.HasNotSetValue(valueType))
-				throw new Exception($"'MauEnumMember.NoSet' must to be in any 'Enum' used as 'MauProperty' type or return of 'MauMethod'. {valueType.FullName}");
+				throw new Exception($"'MauEnumMember.NotSet' must to be in any 'Enum' used as 'MauProperty' type or return of 'MauMethod'. {valueType.FullName}");
 
 			object o = originalValue;
 			string enumValName = valueType.GetFields()
 				.Where(MauEnumMember.HasAttribute)
-				.FirstOrDefault(f => f.GetCustomAttributes<MauEnumMember>(false).First().GetValue().Equals(o))?.Name;
+				.FirstOrDefault(f =>
+				{
+					MauEnumMember f1 = f.GetCustomAttributes<MauEnumMember>(false)
+						.FirstOrDefault(memEnum => memEnum.IsEqual(o));
 
-			originalValue = string.IsNullOrEmpty(enumValName)
-				? Enum.ToObject(valueType, 0) // Set value as 'MauEnumMember.NoSet'
-				: Enum.Parse(valueType, enumValName);
+					return f1 != null && f1.IsEqual(o);
+				})?.Name;
 
+			originalValue = Enum.Parse(valueType, string.IsNullOrEmpty(enumValName) ? "NotSet" : enumValName);
 			return true;
 		}
 
+		public bool IsEqual(object cmpValue)
+		{
+			// Pattern matching
+			return GetValue() switch
+			{
+				string cmpAsStr => (string)cmpValue == cmpAsStr,
+				long cmpAsLong => Convert.ToInt64(cmpValue) == cmpAsLong,
+				_ => false
+			};
+		}
 		public object GetValue()
 		{
 			return Parser switch
